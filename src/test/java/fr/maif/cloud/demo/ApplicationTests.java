32dac4656e2f22869597246978c6951501748962
Chris Woodrow <woodrow.chris@gmail.com>
2018-06-29 11:45:15 +0200

    Fix tests

--- src/test/java/fr/maif/cloud/demo/ApplicationTests.java
+++ src/test/java/fr/maif/cloud/demo/ApplicationTests.java
@@ -1,12 +1,13 @@
 package fr.maif.cloud.demo;
 
+import fr.maif.football.matches.tracker.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
 	@Test
